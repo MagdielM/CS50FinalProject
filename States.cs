a34ac8c09e392ea3ad5a431ddcec66eef1a53d1c
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-05-19 15:02:22 -0400

    Emptied StateMachines.cs. Pending removal. Added CharacterState class to States.cs

---
+++ States.cs
@@ -0,0 +1,16 @@
+﻿using Automatonymous;
+using System;
+namespace States {
+    public class CharacterState {
+        public State Current { get; set; }
+    }
+
+    #region Actions
+    public static class Actions {
+        public static Action IdleBehavior;
+        public static Action RunBehavior;
+        public static Action JumpBehavior;
+        public static Action FallBehavior;
+    }
+    #endregion
+}
