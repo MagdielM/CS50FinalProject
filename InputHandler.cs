194cbcef1d145cbe62a85814581c68a8682ce13c
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-05-27 18:53:53 -0400

    Begun implementing reactive input handler.

---
+++ InputHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Game1 {
+    public static class InputHandler {
+        public static Subject<KeyboardState> KeyboardStates { get; set; }
+        private static IObservable<KeyboardState> distinctKeyboardStates;
+        private static IObservable<IList<KeyboardState>> keyStateBuffer;
+        private static List<Keys> newlyPressedKeys;
+        public static BehaviorSubject<Keys> LatestHorizontalArrowKey { get; set; }
+
+        static InputHandler() {
+            KeyboardStates = new Subject<KeyboardState>();
+            newlyPressedKeys = new List<Keys>();
+            LatestHorizontalArrowKey = new BehaviorSubject<Keys>(Keys.Right);
+            keyStateBuffer = distinctKeyboardStates.Buffer(2, 1);
+        }
+        public void Update() {
+
+        }
+    }
+}
