b2460a094034262d7366695be158f0e504342a03
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-05-28 14:53:16 -0400

    Corrected a few qualifications.

--- General.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace General {
-
-}
