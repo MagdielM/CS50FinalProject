784565bca6106ec1622d46815120092c612853bf
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-06-03 14:41:17 -0400

    Fixed movement keys retriggering every Update().

--- PlayerCharacter.cs
+++ PlayerCharacter.cs
@@ -1,46 +1,76 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace Game1 {
     class PlayerCharacter {
-        public Vector2 Position;
-        AnimationController PlayerAnimator;
+        public Vector2 position;
+        public Vector2 moveSpeed = new Vector2(1.4f, 0);
+        private Keys lastKey;
+        private AnimationController playerAnimator;
 
         public PlayerCharacter(Vector2 spawnPoint) {
-            Position = spawnPoint;
+            position = spawnPoint;
+            playerAnimator = new AnimationController();
         }
 
         public void Initialize() {
         }
 
         public void LoadInit(Dictionary<string, FrameDataLibrary.SpriteReference> spriteSet, string defaultAnimation) {
-            PlayerAnimator.LoadInit(spriteSet, defaultAnimation);
+            playerAnimator.LoadInit(spriteSet, defaultAnimation);
+            InputHandler.LatestHorizontalArrowKey.Subscribe(ManageHorizontalInput);
         }
 
         public void Update(GameTime gameTime) {
-            PlayerAnimator.Update(gameTime);
+            playerAnimator.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch) {
-            PlayerAnimator.Draw(spriteBatch, Position);
+            playerAnimator.Draw(spriteBatch, position);
         }
 
-        public void ManageInput(Keys key) {
+        public void ManageHorizontalInput(Keys key) {
+            switch (key) {
+                case Keys.Left:
+                    position -= moveSpeed;
+                    if (key != lastKey) {
+                        Flip();
+                        playerAnimator.SetTag("Run");
+                    }
+                    break;
+                case Keys.Right:
+                    position += moveSpeed;
+                    if (key != lastKey) {
+                        Deflip();
+                        playerAnimator.SetTag("Run");
+                    }
+                    break;
+                default:
+                    if (key != lastKey) {
+                        playerAnimator.SetTag("Idle");
+                    }
+                    break;
+            }
+            lastKey = key;
+        }
+
+        public void ManageVerticalInput(Keys key) {
 
         }
 
         public void Flip() {
-            PlayerAnimator.SetCategory("flipped", 
-                PlayerAnimator.ActiveAnimation.FrameData.ActiveTagName, 
-                PlayerAnimator.ActiveAnimation.FrameData.ActiveTag.StartFrame);
+            playerAnimator.SetCategory("flipped",
+                playerAnimator.ActiveAnimation.FrameData.ActiveTagName,
+                playerAnimator.ActiveAnimation.FrameData.ActiveTag.StartFrame);
         }
 
         public void Deflip() {
-            PlayerAnimator.SetCategory("normal",
-                PlayerAnimator.ActiveAnimation.FrameData.ActiveTagName,
-                PlayerAnimator.ActiveAnimation.FrameData.ActiveTag.StartFrame);
+            playerAnimator.SetCategory("normal",
+                playerAnimator.ActiveAnimation.FrameData.ActiveTagName,
+                playerAnimator.ActiveAnimation.FrameData.ActiveTag.StartFrame);
         }
     }
 }
