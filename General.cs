26e2082dc4344b74e89f0c8ecbd9dd2ac4a39ca5
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-06-23 12:29:13 -0400

    Re-added General class for general functions.

---
+++ General.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using RectangleFLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1 {
+    class General {
+        public static Rectangle RoundRectanglePoints(RectangleF r) {
+            return new Rectangle((int)Math.Round(r.X), (int)Math.Round(r.Y), (int)Math.Round(r.Width), (int)Math.Round(r.Height));
+        }
+    }
+}
