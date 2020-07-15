1ad223d729c2009417940825562206b702e95a1f
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-07-15 15:19:24 -0400

    Integrated PostSharp for custom parameter contracts and other features.

---
+++ Contracts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using PostSharp;
+using PostSharp.Aspects;
+using PostSharp.Patterns.Contracts;
+using PostSharp.Reflection;
+
+namespace Game1 {
+    public class NonEmptyAttribute : LocationContractAttribute,
+        ILocationValidationAspect<Point>,
+        ILocationValidationAspect<Rectangle> {
+        public NonEmptyAttribute() : base() {}
+
+        public Exception ValidateValue(Point value, string locationName, LocationKind locationKind, LocationValidationContext context) {
+            if (value == Point.Zero)
+                return new ArgumentOutOfRangeException($"The coordinates for {locationName} must not be (0, 0).");
+            else
+                return null;
+        }
+
+        public Exception ValidateValue(Rectangle value, string locationName, LocationKind locationKind, LocationValidationContext context) {
+            if (value.Size == Point.Zero)
+                return new ArgumentOutOfRangeException($"The size of {value} must not be 0.");
+            else
+                return null;
+        }
+    }
+}
