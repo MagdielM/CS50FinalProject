8495236f66d2e89209cd618294bc7d109cbb2c6b
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-07-15 12:06:04 -0400

    Added rudimentary camera system.

--- Camera.cs
+++ Camera.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Game1 {
-    class Camera {
-        public Matrix Transform { get; private set; }
+    internal class Camera {
+        public Matrix Transform { get; internal set; }
 
         public void Follow(PlayerCharacter target) {
             Matrix offset = Matrix.CreateTranslation(MainGame.virtualWidth / 2, MainGame.virtualHeight / 2, 0);
             Matrix position = Matrix.CreateTranslation(
-                -target.playerBounds.Location.X - target.playerBounds.Width / 2, 
-                -target.playerBounds.Location.Y - target.playerBounds.Height / 2, 
+                -target.playerBounds.Location.X - (target.playerBounds.Width / 2),
+                -target.playerBounds.Location.Y - (target.playerBounds.Height / 2),
                 0);
             Transform = position * offset;
         }
