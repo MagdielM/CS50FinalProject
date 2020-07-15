66083ca416599bc5595f0fa7e51b4144ce2b755e
Magdiel Marrero <magdiel.marrero.033@gmail.com>
2020-07-15 15:46:00 -0400

    Added rudimentary game state system and pause screen,

--- MainGame.cs
+++ MainGame.cs
@@ -2,26 +2,41 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SliceDataLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using static Game1.Level;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using static Game1.Canvas;
 
 namespace Game1 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
     class MainGame : Game {
-
         #region General References
 
         public const int virtualWidth = 384;
         public const int virtualHeight = 216;
-        Rectangle screen;
-        RenderTarget2D renderTarget;
-        readonly GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        public const int preferredBackBufferWidth = 1280;
+        public const int preferredBackBufferHeight = 720;
         public float fontHeight;
-        public Vector2[] debugLinePositions;
+        private KeyboardState previousKeyboardState;
+        private Rectangle screen;
+        private Rectangle virtualBounds;
+        private Vector2[] debugLinePositions;
+        public SpriteBatch spriteBatch;
+        readonly GraphicsDeviceManager graphics;
+        private Texture2D blackScreenFilter;
+        private RenderTarget2D renderTarget;
+        public Cursor cursor;
+        private Canvas pauseScreenCanvas;
+        public static Subject<GlobalEvent> globalEventStream = new Subject<GlobalEvent>();
+        private GlobalState globalState = GlobalState.Running;
+
+
+
         #endregion
 
         #region Game References
@@ -32,14 +47,48 @@ namespace Game1 {
         public static Vector2 fallingGravity = new Vector2(0, 26f);
         public static Level currentLevel;
 
+        #region Global States and Events
+
+        public enum GlobalState {
+            AtStart,
+            Running,
+            Paused,
+            Ending
+        }
+
+        public enum GlobalEvent {
+            Start,
+            Resume,
+            Pause,
+            End
+        }
+
+        #endregion
+
         #endregion
 
         #region Content References
 
         Texture2D background1;
         private SpriteFont font;
-        Dictionary<string, SpriteReference> playerSpriteSet;
-        private string level1TilemapPath = "Sprites/PlatformerPack/raw files/tilemap";
+        Dictionary<string, FrameReference> playerSpriteSet;
+        private const string level1TilemapPath = "Sprites/PlatformerPack/raw files/tilemap";
+        private const string defaultUiAtlasPath = "Sprites/PlatformerPack/raw files/ui_3x3";
+        private const string defaultUiDataPath = "Sprites/PlatformerPack/raw files/ui_3x3_data";
+        private const string cursorPath = "Sprites/PlatformerPack/raw files/cursor";
+        private const string playerFrameDataPath = "Sprites/PlatformerPack/Player/player_frame_data";
+        private const string background1Path = "Sprites/Assets/Background_1";
+        private const string pixelFontPath = "Sprites/PixelFont";
+
+        #region UI Resource Paths
+        private const string lineHorizontal = "line_horizontal";
+        private const string lineHorizontalStart = "line_horizontal_start";
+        private const string lineHorizontalEnd = "line_horizontal_end";
+        private const string lineVertical = "line_vertical";
+        private const string lineVerticalStart = "line_vertical_start";
+        private const string lineVerticalEnd = "line_vertical_end";
+        private const string genericCorner = "corner";
+        #endregion
 
         #endregion
 
@@ -57,12 +106,19 @@ namespace Game1 {
         /// </summary>
         protected override void Initialize() {
             Player = new PlayerCharacter(new Point(56, 256));
-            graphics.PreferredBackBufferWidth = 1280;
-            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = preferredBackBufferWidth;
+            graphics.PreferredBackBufferHeight = preferredBackBufferHeight;
             graphics.ApplyChanges();
+            blackScreenFilter = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Color[] c = new Color[1];
+            c[0] = Color.White * 0.55f;
+            blackScreenFilter.SetData(c);
             renderTarget = new RenderTarget2D(GraphicsDevice, virtualWidth, virtualHeight);
-            screen = new Rectangle(new Point(0, 0), new Point(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight));
+            screen = new Rectangle(Point.Zero, new Point(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight));
+            virtualBounds = new Rectangle(Point.Zero, new Point(virtualWidth, virtualHeight));
             camera = new Camera();
+            previousKeyboardState = Keyboard.GetState();
+            globalEventStream.Subscribe(ReactToEvent);
             base.Initialize();
         }
 
@@ -74,9 +130,40 @@ namespace Game1 {
 
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            background1 = Content.Load<Texture2D>("Sprites/Assets/Background_1");
-            font = Content.Load<SpriteFont>("Sprites/PixelFont");
-            fontHeight = font.MeasureString("Height test").Y;
+            background1 = Content.Load<Texture2D>(background1Path);
+            font = Content.Load<SpriteFont>(pixelFontPath);
+            fontHeight = font.MeasureString("A").Y;
+
+            SliceReference uiTexture = new SliceReference(
+                Content.Load<Texture2D>(defaultUiAtlasPath),
+                Content.Load<SliceData>(defaultUiDataPath));
+
+            pauseScreenCanvas = new Canvas(ref uiTexture);
+            pauseScreenCanvas.AddChild(
+                new BoxElement(
+                    ref uiTexture, new Rectangle(0, 0, 96, 128),
+                    new LineElement(
+                        ref uiTexture, false,
+                        new RectangleElement(
+                            ref uiTexture, lineHorizontal),
+                        new RectangleElement(
+                            ref uiTexture, lineHorizontalStart),
+                        new RectangleElement(
+                            ref uiTexture, lineHorizontalEnd)),
+                    new LineElement(
+                        ref uiTexture, true,
+                        new RectangleElement(
+                            ref uiTexture, lineVertical),
+                        new RectangleElement(
+                            ref uiTexture, lineVerticalStart),
+                        new RectangleElement(
+                            ref uiTexture, lineVerticalEnd)),
+                    new RectangleElement(
+                        ref uiTexture, genericCorner)));
+            pauseScreenCanvas.Children[0].Centralize();
+            
+            cursor = new Cursor(Content.Load<Texture2D>(cursorPath));
+
             debugLinePositions = new Vector2[] {
                 Vector2.Zero,
                 new Vector2(0, fontHeight),
@@ -86,14 +173,13 @@ namespace Game1 {
                 new Vector2(0, fontHeight * 5),
                 new Vector2(0, fontHeight * 6)
             };
+            currentLevel = new Level(Content.Load<Texture2D>(level1TilemapPath), LevelDefinitions.level1Tiles);
 
-            currentLevel = new Level(Content.Load<Texture2D>(level1TilemapPath), LevelDefinitions.level1);
-
-            var playerData = Content.Load<FrameData>("Sprites/PlatformerPack/Player/player_frame_data");
-            playerSpriteSet = new Dictionary<string, SpriteReference> {
+            var playerData = Content.Load<FrameData>(playerFrameDataPath);
+            playerSpriteSet = new Dictionary<string, FrameReference> {
                 {
                     "Main",
-                    new SpriteReference(
+                    new FrameReference(
                 Content.Load<Texture2D>(playerData.ImagePath), playerData)
                 }
             };
@@ -115,17 +201,29 @@ namespace Game1 {
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime) {
+            KeyboardState currentKeyboardState = Keyboard.GetState();
+            switch (globalState) {
+                case GlobalState.Running:
+                    Player.Update(gameTime);
+                    foreach (Keys key in InputHandler.InputOutKeys.Value) {
+                        Player.ManageGeneralInput(key);
+                    }
+                    camera.Follow(Player);
+                    InputHandler.Update();
+                    if (currentKeyboardState.IsKeyDown(Keys.Escape) && previousKeyboardState.IsKeyUp(Keys.Escape)) {
+                        globalEventStream.OnNext(GlobalEvent.Pause);
+                    }
+                    base.Update(gameTime);
+                    break;
 
-            // Checks for held keys
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-/*            foreach (Keys key in InputHandler.InputOutKeys.Value) {
-                Player.ManageGeneralInput(key);
-            }*/
-            Player.Update(gameTime);
-            camera.Follow(Player);
-            InputHandler.Update();
-            base.Update(gameTime);
+                case GlobalState.Paused:
+                    cursor.Update();
+                    if (currentKeyboardState.IsKeyDown(Keys.Escape) && previousKeyboardState.IsKeyUp(Keys.Escape)) {
+                        globalEventStream.OnNext(GlobalEvent.Resume);
+                    }
+                    break;
+            }
+            previousKeyboardState = currentKeyboardState;
         }
 
         /// <summary>
@@ -150,6 +248,17 @@ namespace Game1 {
             Player.Draw(spriteBatch);
             spriteBatch.End();
 
+            if (globalState == GlobalState.Paused) {
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp,
+                    null, null, null, null);
+                spriteBatch.Draw(blackScreenFilter, virtualBounds, Color.Black);
+                pauseScreenCanvas.Draw(spriteBatch);
+                if (!cursor.isOutdated) {
+                    cursor.Draw(spriteBatch);
+                }
+                spriteBatch.End();
+            }
+
             // Draw internal render target to back buffer.
             GraphicsDevice.SetRenderTarget(null);
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp);
@@ -158,8 +267,21 @@ namespace Game1 {
             spriteBatch.DrawString(font, $"surroundingTiles: {Player.surroundingTiles.Count}", debugLinePositions[1], Color.Black);
             spriteBatch.DrawString(font, $"levelTiles: {currentLevel.TileArray.Count}", debugLinePositions[2], Color.Black);
             spriteBatch.End();
-
             base.Draw(gameTime);
+        }
+
+        private void ReactToEvent(GlobalEvent globalEvent) {
+            switch (globalEvent) {
+                case GlobalEvent.Pause:
+                    previousKeyboardState = Keyboard.GetState();
+                    globalState = GlobalState.Paused;
+                    break;
+
+                case GlobalEvent.Resume:
+                    cursor.isOutdated = true;
+                    globalState = GlobalState.Running;
+                    break;
+            }
         }
     }
 }
